4c8048eabb98f653556a37b0a31c635b85f6dc21
nagarajmanjunath <nagarajmanjunath22@gmail.com>
2021-12-15 12:23:10 +0800

    param receive as serde:value

--- src/request.rs
+++ src/request.rs
@@ -1,30 +1,30 @@
 use crate::client::Client;
 use crate::error::Result;
 use async_trait::async_trait;
-use jsonrpc_core::types::params::Params;
+use serde_json::Value;
 
 #[async_trait]
 pub trait HttpRequest {
-    async fn get_api(&self, request_url: &str, params: &Params) -> Result<serde_json::Value>;
+    async fn get_api(&self, request_url: &str, params: &Value) -> Result<serde_json::Value>;
 
-    async fn post_api(&self, request_url: &str, params: &Params) -> Result<serde_json::Value>;
+    async fn post_api(&self, request_url: &str, params: &Value) -> Result<serde_json::Value>;
 
-    async fn delete_api(&self, request_url: &str, params: &Params) -> Result<serde_json::Value>;
+    async fn delete_api(&self, request_url: &str, params: &Value) -> Result<serde_json::Value>;
 }
 
 #[async_trait]
 impl HttpRequest for Client {
-    async fn get_api(&self, request_url: &str, params: &Params) -> Result<serde_json::Value> {
+    async fn get_api(&self, request_url: &str, params: &Value) -> Result<serde_json::Value> {
         log::trace!("request url {:?}", request_url);
         self.get(request_url, params).await
     }
 
-    async fn post_api(&self, request_url: &str, params: &Params) -> Result<serde_json::Value> {
+    async fn post_api(&self, request_url: &str, params: &Value) -> Result<serde_json::Value> {
         log::trace!("request url {:?}", request_url);
         self.post(request_url, params).await
     }
 
-    async fn delete_api(&self, request_url: &str, params: &Params) -> Result<serde_json::Value> {
+    async fn delete_api(&self, request_url: &str, params: &Value) -> Result<serde_json::Value> {
         log::trace!("request url {:?}", request_url);
         self.delete(request_url, params).await
     }
